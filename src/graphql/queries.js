9f380445892f03ac764c9c29cfba1ed30c9bb98f
Timo Varila <timo.varila@siili.com>
2019-11-29 19:58:36 +0200

    1st

---
+++ src/graphql/queries.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = `query GetNote($id: ID!) {
+  getNote(id: $id) {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
+export const listNotes = `query ListNotes(
+  $filter: ModelNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      clientId
+      name
+      description
+      completed
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
